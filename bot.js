f4e2ce8f8fb0e44d13a8675223afaec20d1c6fec
Basim <basim.abdullah247@gmail.com>
2021-02-02 17:30:45 +0500

    gif 0.1

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 var search = require('youtube-search');
 const ytdl = require("ytdl-core");
 const queue = new Map();
+const fetch = require('node-fetch');
 
 client.login(process.env.BOTTOKEN);
 
@@ -79,10 +80,10 @@ client.on('message', async msg => {
             result = 'Tails';
         const embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
-            .setAuthor('By Basim', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
+            .setAuthor('Coinflip', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
             .addFields({ name: 'Coinflip Result: ', value: result })
             .setTimestamp()
-            .setFooter('Coinflip By Basim');
+            .setFooter('Mera Bot By Basim');
 
         msg.channel.send(embed);
     }
@@ -93,10 +94,10 @@ client.on('message', async msg => {
             if (!args.length) {
                 const embed = new Discord.MessageEmbed()
                     .setColor('#ff0000')
-                    .setAuthor('By Basim', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                    .setAuthor('Magic 8Ball', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
                     .addFields({ name: 'Error: ', value: 'You must include a statement.' })
                     .setTimestamp()
-                    .setFooter('Magic 8Ball By Basim');
+                    .setFooter('Mera Bot By Basim');
                 msg.channel.send(embed);
             }
         } else {
@@ -105,160 +106,39 @@ client.on('message', async msg => {
 
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('By Basim', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Magic 8Ball', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
                 .addFields({ name: 'Magic 8ball Says: ', value: result })
                 .setTimestamp()
-                .setFooter('Magic 8Ball By Basim');
+                .setFooter('Mera Bot By Basim');
             msg.channel.send(embed);
         }
     }
-    if (msg.content.startsWith('test')) {
-        const connection = await msg.member.voice.channel.join();
-        const args = msg.content.slice(4).trim().split(' ');
+    if (msg.content.startsWith('bot gif')) {
+        let url = `https://api.tenor.com/v1/search?q=laugh&key=${process.env.TENORKEY}&limit=8`
+        const args = msg.content.slice(7).trim().split(' ');
         const command = args.shift().toLowerCase();
-        const serverQueue = queue.get(msg.guild.id);
-        var dispatcher;
-        var link;
-        if (command == '') {
+        if (command === '') {
             if (!args.length) {
                 const embed = new Discord.MessageEmbed()
                     .setColor('#ff0000')
-                    .setAuthor('By Basim', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                    .addFields({ name: 'Error: ', value: 'Song name' })
+                    .setAuthor('Gif', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                    .addFields({ name: 'Error: ', value: 'You must include a gif name' })
                     .setTimestamp()
-                    .setFooter('Music player by Basim');
+                    .setFooter('Mera Bot By Basim');
                 msg.channel.send(embed);
             }
         } else {
-            console.log('something');
-            execute(msg, serverQueue);
-            /*var opts = {
-                maxResults: 3,
-                key: process.env.YTKEY
-            };
-            search(command, opts, function(err, results) {
-                if (err) return console.log(err);
-                link = results[0].link;
-            });
-            search(command, opts, function(err, results) {
-                console.log(link);
-                dispatcher = connection.play(link);
-                dispatcher.on('start', () => {
-                    console.log('audio.mp3 is now playing!');
-                });
-
-                dispatcher.on('finish', () => {
-                    console.log('audio.mp3 has finished playing!');
-                    dispatcher.destroy();
-                });
-                // Always remember to handle errors appropriately!
-                dispatcher.on('error', console.error);
-            });*/
+            let response = await fetch(url);
+            let json = await response.json();
+            console.log(json);
+            const embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setAuthor('Gif', 'https://i.imgur.com/zwg237E.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .addFields({ name: 'GIF: ', value: json.results[0].url })
+                .setTimestamp()
+                .setFooter('Mera Bot By Basim');
+            msg.channel.send(embed);
         }
-
     }
 
 });
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-    var link;
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-    var opts = {
-        maxResults: 3,
-        key: process.env.YTKEY
-    };
-    search(args[1], opts, async function(err, results) {
-        if (err) return console.log(err);
-        link = results[0].link;
-        //console.log(link);
-
-        console.log(link);
-        args[1] = 'https://www.youtube.com/watch?v=WNeLUngb-Xg';
-        const songInfo = await ytdl.getInfo(link);
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-        };
-
-        if (!serverQueue) {
-            const queueContruct = {
-                textChannel: message.channel,
-                voiceChannel: voiceChannel,
-                connection: null,
-                songs: [],
-                volume: 5,
-                playing: true
-            };
-
-            queue.set(message.guild.id, queueContruct);
-
-            queueContruct.songs.push(song);
-
-            try {
-                console.log('play');
-                var connection = await voiceChannel.join();
-                queueContruct.connection = connection;
-                play(message.guild, queueContruct.songs[0]);
-            } catch (err) {
-                console.log(err);
-                queue.delete(message.guild.id);
-                return message.channel.send(err);
-            }
-        } else {
-            serverQueue.songs.push(song);
-            return message.channel.send(`${song.title} has been added to the queue!`);
-        }
-    });
-}
-
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could stop!");
-
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
