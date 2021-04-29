a8119c57d567c6056c6b590a7a38e3ffc00ca2e3
Basim <basim.abdullah247@gmail.com>
2021-04-29 17:25:52 +0500

    command handler, bot gif

--- bot.js
+++ bot.js
@@ -21,209 +21,20 @@ app.listen(port, function() {
 
 /// Bot
 client.login(process.env.BOTTOKEN);
-var ballArray = ['As I see it, yes.',
-    'Ask again later.',
-    'Better not tell you now.',
-    'Cannot predict now.',
-    'Concentrate and ask again.',
-    'Don’t count on it.',
-    'It is certain.',
-    'It is decidedly so.',
-    'Most likely.',
-    'My reply is no.',
-    'My sources say no.',
-    'Outlook not so good.',
-    'Outlook good.',
-    'Reply hazy, try again.',
-    'Signs point to yes.',
-    'Very doubtful.',
-    'Without a doubt.',
-    'Yes.',
-    'Yes – definitely.',
-    'You may rely on it.'
-];
-var firstID, secondID, firstChoice, secondChoice;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     client.user.setActivity({
-        //status: "online", //You can show online, idle....
-        /*game: {
-            name: "with life", //The message shown
-            type: "Playing" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }*/
-        name: 'with life',
+        name: 'with life (bot help)',
         options: 'Playing'
     });
 });
-var idx = 0;
-client.on('message', async msg => {
-    if (msg.content === 'ahad') {
-        msg.channel.send('ahad is bad boi');
-    }
-    if (msg.content === 'gud morning guyz') {
-        msg.channel.send('come again CR');
-    }
-    if (msg.content === 'come again CR') {
-        msg.channel.send('yes sir we can hear u');
-    }
-    if (msg.content === 'yes sir') {
-        msg.channel.send('ahad sir ka uthao mat! PLSSS');
-    }
-    if (msg.content === 'bye') {
-        msg.channel.send('ciya guyz');
-    }
-    if (msg.content === 'faisal') {
-        msg.channel.send('yaar mujha bhej me copy nae krun ga');
-    }
-    if (msg.content === 'Talha sajjad?') {
-        msg.channel.send('sir mic issue');
-    }
-    if (msg.content === 'kazmi yeh white hai?') {
-        msg.channel.send('yaar yeh white kiun hai?');
-    }
-    if (msg.content === 'kazmi yahan aa') {
-        msg.channel.send('yaar kahan ana hai');
-    }
-    if (msg.content === 'bot coinflip') {
-        var chance = Math.floor((Math.random() * 100) + 0);
-        var result;
-        console.log(chance);
-        if (chance % 2 == 0)
-            result = 'Heads';
-        else
-            result = 'Tails';
-        const embed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setAuthor('Coinflip', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-            .addFields({ name: 'Coinflip Result: ', value: result })
-            .setTimestamp()
-            .setFooter('Mera Bot By Basim');
 
-        msg.channel.send(embed);
-    }
-    if (msg.content.startsWith('bot 8ball')) {
-        const args = msg.content.slice(9).trim().split(' ');
-        const command = args.shift().toLowerCase();
-        if (command === '') {
-            if (!args.length) {
-                const embed = new Discord.MessageEmbed()
-                    .setColor('#ff0000')
-                    .setAuthor('Magic 8Ball', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                    .addFields({ name: 'Error: ', value: 'You must include a statement.' })
-                    .setTimestamp()
-                    .setFooter('Mera Bot By Basim');
-                msg.channel.send(embed);
-            }
-        } else {
-            var chance = Math.floor((Math.random() * 20) + 1);
-            var result = ballArray[chance];
+const commandHandler = require("./commands");
+client.on('message', commandHandler);
 
-            const embed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setAuthor('Magic 8Ball', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                .addFields({ name: 'Magic 8ball Says: ', value: result })
-                .setTimestamp()
-                .setFooter('Mera Bot By Basim');
-            msg.channel.send(embed);
-        }
-    }
-    if (msg.content === 'bot server info') {
-        const embed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setAuthor('Server Info', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-            .setImage(msg.guild.iconURL)
-            .setDescription(`${msg.guild}'s information`)
-            .addField("Owner", `${msg.guild.owner}`)
-            .addField("Member Count", `${msg.guild.memberCount} members`)
-            .addField("Roles", `${msg.guild.roles.cache.size} roles`)
-            .setFooter('Mera Bot By Basim');
-        msg.channel.send(embed);
-    }
-    if (msg.content === 'bot my info') {
-        var d1 = new Date(msg.author.createdTimestamp);
-        date = d1.toDateString();
-        const embed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setAuthor('User Info', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-            .setThumbnail(msg.author.avatarURL)
-            .setDescription(`${msg.author}'s information`)
-            .addField("Username", `${msg.author.tag}`)
-            .addField("Account created:", `${date}`)
-            .setFooter('Mera Bot By Basim');
-        msg.channel.send(embed);
-    }
-});
-/// Logs
-client.on('message', async(message) => {
-    if (message.content === 'bot logs') {
-        var entry = await message.guild.fetchAuditLogs({ type: 'MEMBER_DISCONNECT' }).then(audit => audit.entries.array())
-        var i = 0;
-        const d2 = new Date();
-        date2 = d2.toDateString(); // current date
-
-        message.channel.send('`========DISCONNECT LOGS========`');
-        while (entry[i]) {
-            if (entry[i].action == 'MEMBER_DISCONNECT') {
-                var d1 = new Date(entry[i].createdTimestamp);
-                date1 = d1.toDateString();
-                if (date1 == date2) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setAuthor('Disconnect Logs', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                        .addFields({ name: 'Logs', value: (`\`${entry[i].executor.username} disconneted a user\``) })
-                        .setTimestamp(entry[i].createdTimestamp)
-                        .setFooter('Mera Bot By Basim');
-                    message.channel.send(embed);
-                }
-            }
-            i++;
-        }
-        console.log(i);
-        entry = await message.guild.fetchAuditLogs({ type: 'MEMBER_MOVE' }).then(audit => audit.entries.array())
-        i = 0;
-        message.channel.send('`========MOVE LOGS========`');
-        while (entry[i]) {
-            if (entry[i].action == 'MEMBER_MOVE') {
-                d1 = new Date(entry[i].createdTimestamp);
-                date1 = d1.toDateString();
-                if (date1 == date2) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setAuthor('Move Logs', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                        .addFields({ name: 'Logs', value: (`\`${entry[i].executor.username} moved a user to ${entry[i].extra.channel.name}\``) })
-                        .setTimestamp(entry[i].createdTimestamp)
-                        .setFooter('Mera Bot By Basim');
-                    message.channel.send(embed);
-                }
-                i++;
-            }
-        }
-        /*entry = await message.guild.fetchAuditLogs({ type: 'MEMBER_UPDATE' }).then(audit => audit.entries.array())
-        i = 0;
-        message.channel.send('`========UPDATE LOGS========`');
-        while (entry[i]) {
-            if (entry[i].action == 'MEMBER_UPDATE') {
-                d1 = new Date(entry[i].createdTimestamp);
-                date1 = d1.toDateString();
-                if (date1 == date2) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setAuthor('Update Logs', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
-                        .addFields({ name: 'Logs', value: (`\`${entry[i].executor.username} changed name from ${entry[i].changes[i].old} to ${entry[i].changes[i].new}\``) })
-                        .setTimestamp(entry[i].createdTimestamp)
-                        .setFooter('Mera Bot By Basim');
-                    message.channel.send(embed);
-                }
-                i++;
-            }
-        }*/
-        message.channel.send('`========END LOGS========`');
-    }
-});
-//// RPS
-client.on('message', msg => {
+client.on('message', async(msg) => {
     if (msg.content.startsWith('bot rps')) {
         const args = msg.content.slice(7).trim().split(' ');
         const command = args.shift().toLowerCase();
@@ -231,7 +42,7 @@ client.on('message', msg => {
             if (!args.length) {
                 const embed = new Discord.MessageEmbed()
                     .setColor('#ff0000')
-                    .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                    .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                     .addFields({ name: 'Error: ', value: 'You must tag another person to play with.' })
                     .setTimestamp()
                     .setFooter('Mera Bot By Basim');
@@ -282,7 +93,7 @@ client.on('message', msg => {
         if (firstChoice === secondChoice) {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `Both <@${firstID}> and <@${secondID}> choose ${firstChoice} \n\n Its a Draw!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -293,7 +104,7 @@ client.on('message', msg => {
         if (firstChoice === 'scissors' && secondChoice == 'paper') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${firstID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -304,7 +115,7 @@ client.on('message', msg => {
         if (firstChoice === 'scissors' && secondChoice == 'rock') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${secondID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -315,7 +126,7 @@ client.on('message', msg => {
         if (firstChoice === 'paper' && secondChoice == 'rock') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${firstID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -326,7 +137,7 @@ client.on('message', msg => {
         if (firstChoice === 'paper' && secondChoice == 'scissors') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${secondID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -337,7 +148,7 @@ client.on('message', msg => {
         if (firstChoice === 'rock' && secondChoice == 'paper') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${secondID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -348,7 +159,7 @@ client.on('message', msg => {
         if (firstChoice === 'rock' && secondChoice == 'scissors') {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
-                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'https://basimabdullahtariq.azurewebsites.net/')
+                .setAuthor('Rock/Paper/Scissors', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
                 .addFields({ name: 'Result: ', value: `<@${firstID}> choose ${firstChoice} \n <@${secondID}> choose ${secondChoice} \n\n <@${firstID}> Won!` })
                 .setTimestamp()
                 .setFooter('Mera Bot By Basim');
@@ -357,23 +168,4 @@ client.on('message', msg => {
             secondChoice = null;
         }
     }
-    if (msg.content == 'check jail') {
-        //if (msg.member.voice.channel) {
-        //const connection = msg.member.voice.channel.join();
-        //}
-        const channel = msg.guild.channels.cache.get('783632677793562684');
-        channel.members.forEach(member => {
-            console.log(member.roles.highest.name);
-            if (member.roles.highest.name == "Jail") {
-                //Rest of your code
-                member.voice.setMute(true);
-                member.voice.setDeaf(true);
-                member.voice.setChannel('826831011227631638');
-            }
-        })
-        setTimeout(function() {
-            client.channels.cache.get('806436951561076736').send("check jail");
-        }, 10000);
-    }
 });
-//// Jail system
---
+++ commands.js
@@ -0,0 +1,214 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+var ballArray = ['As I see it, yes.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Don’t count on it.',
+    'It is certain.',
+    'It is decidedly so.',
+    'Most likely.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Outlook good.',
+    'Reply hazy, try again.',
+    'Signs point to yes.',
+    'Very doubtful.',
+    'Without a doubt.',
+    'Yes.',
+    'Yes – definitely.',
+    'You may rely on it.'
+];
+var firstID, secondID, firstChoice, secondChoice, idx;
+module.exports = async function(msg) {
+    if (msg.content === 'ahad') {
+        msg.channel.send('ahad is bad boi');
+    }
+    if (msg.content === 'gud morning guyz') {
+        msg.channel.send('come again CR');
+    }
+    if (msg.content === 'come again CR') {
+        msg.channel.send('yes sir we can hear u');
+    }
+    if (msg.content === 'yes sir') {
+        msg.channel.send('ahad sir ka uthao mat! PLSSS');
+    }
+    if (msg.content === 'bye') {
+        msg.channel.send('ciya guyz');
+    }
+    if (msg.content === 'faisal') {
+        msg.channel.send('yaar mujha bhej me copy nae krun ga');
+    }
+    if (msg.content === 'Talha sajjad?') {
+        msg.channel.send('sir mic issue');
+    }
+    if (msg.content === 'kazmi yeh white hai?') {
+        msg.channel.send('yaar yeh white kiun hai?');
+    }
+    if (msg.content === 'kazmi yahan aa') {
+        msg.channel.send('yaar kahan ana hai');
+    }
+    if (msg.content === 'bot coinflip') {
+        var chance = Math.floor((Math.random() * 100) + 0);
+        var result;
+        console.log(chance);
+        if (chance % 2 == 0)
+            result = 'Heads';
+        else
+            result = 'Tails';
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor('Coinflip', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+            .addFields({ name: 'Coinflip Result: ', value: result })
+            .setTimestamp()
+            .setFooter('Mera Bot By Basim');
+
+        msg.channel.send(embed);
+    }
+    if (msg.content.startsWith('bot 8ball')) {
+        const args = msg.content.slice(9).trim().split(' ');
+        const command = args.shift().toLowerCase();
+        if (command === '') {
+            if (!args.length) {
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#ff0000')
+                    .setAuthor('Magic 8Ball', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+                    .addFields({ name: 'Error: ', value: 'You must include a statement.' })
+                    .setTimestamp()
+                    .setFooter('Mera Bot By Basim');
+                msg.channel.send(embed);
+            }
+        } else {
+            var chance = Math.floor((Math.random() * 20) + 1);
+            var result = ballArray[chance];
+
+            const embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setAuthor('Magic 8Ball', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+                .addFields({ name: 'Magic 8ball Says: ', value: result })
+                .setTimestamp()
+                .setFooter('Mera Bot By Basim');
+            msg.channel.send(embed);
+        }
+    }
+    if (msg.content === 'bot server info') {
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor('Server Info', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+            .setImage(msg.guild.iconURL)
+            .setDescription(`${msg.guild}'s information`)
+            .addField("Owner", `${msg.guild.owner}`)
+            .addField("Member Count", `${msg.guild.memberCount} members`)
+            .addField("Roles", `${msg.guild.roles.cache.size} roles`)
+            .setFooter('Mera Bot By Basim');
+        msg.channel.send(embed);
+    }
+    if (msg.content === 'bot my info') {
+        var d1 = new Date(msg.author.createdTimestamp);
+        date = d1.toDateString();
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor('User Info', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+            .setThumbnail(msg.author.avatarURL)
+            .setDescription(`${msg.author}'s information`)
+            .addField("Username", `${msg.author.tag}`)
+            .addField("Account created:", `${date}`)
+            .setFooter('Mera Bot By Basim');
+        msg.channel.send(embed);
+    }
+    if (msg.content === 'bot logs') {
+        var entry = await msg.guild.fetchAuditLogs({ type: 'MEMBER_DISCONNECT' }).then(audit => audit.entries.array())
+        var i = 0;
+        const d2 = new Date();
+        date2 = d2.toDateString(); // current date
+
+        msg.channel.send('`========DISCONNECT LOGS========`');
+        while (entry[i]) {
+            if (entry[i].action == 'MEMBER_DISCONNECT') {
+                var d1 = new Date(entry[i].createdTimestamp);
+                date1 = d1.toDateString();
+                if (date1 == date2) {
+                    const embed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setAuthor('Disconnect Logs', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+                        .addFields({ name: 'Logs', value: (`\`${entry[i].executor.username} disconneted a user\``) })
+                        .setTimestamp(entry[i].createdTimestamp)
+                        .setFooter('Mera Bot By Basim');
+                    msg.channel.send(embed);
+                }
+            }
+            i++;
+        }
+        console.log(i);
+        entry = await msg.guild.fetchAuditLogs({ type: 'MEMBER_MOVE' }).then(audit => audit.entries.array())
+        i = 0;
+        msg.channel.send('`========MOVE LOGS========`');
+        while (entry[i]) {
+            if (entry[i].action == 'MEMBER_MOVE') {
+                d1 = new Date(entry[i].createdTimestamp);
+                date1 = d1.toDateString();
+                if (date1 == date2) {
+                    const embed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setAuthor('Move Logs', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+                        .addFields({ name: 'Logs', value: (`\`${entry[i].executor.username} moved a user to ${entry[i].extra.channel.name}\``) })
+                        .setTimestamp(entry[i].createdTimestamp)
+                        .setFooter('Mera Bot By Basim');
+                    msg.channel.send(embed);
+                }
+                i++;
+            }
+        }
+        msg.channel.send('`========END LOGS========`');
+    }
+    if (msg.content == 'check jail') {
+        const channel = msg.guild.channels.cache.get('783632677793562684');
+        channel.members.forEach(member => {
+            console.log(member.roles.highest.name);
+            if (member.roles.highest.name == "Jail") {
+                //Rest of your code
+                member.voice.setMute(true);
+                member.voice.setDeaf(true);
+                member.voice.setChannel('826831011227631638');
+            }
+        })
+        setTimeout(function() {
+            client.channels.cache.get('806436951561076736').send("check jail");
+        }, 10000);
+    }
+    if (msg.content.startsWith("bot gif")) {
+        const args = msg.content.slice(7).trim().split(' ');
+        const command = args.shift().toLowerCase();
+        var url;
+        if (command == '')
+            url = `https://g.tenor.com/v1/search?q=excited&key=${process.env.TENORKEY}&limit=8`;
+        else
+            url = `https://g.tenor.com/v1/search?q=${command}&key=${process.env.TENORKEY}&limit=8`;
+        let response = await fetch(url);
+        let json = await response.json();
+        let seed = Math.floor(Math.random() * json.results.length);
+        msg.channel.send(json.results[seed].url);
+    }
+    if (msg.content == "bot help") {
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor('Help', 'https://i.imgur.com/L04qJk6.png', 'http://basimabdullahtariq.tk/')
+            .addFields({ name: 'Commands', value: (`bot help
+                                                    bot logs
+                                                    bot 8ball 
+                                                    \`usage bot 8ball statement\`
+                                                    bot coinflip
+                                                    bot gif
+                                                    \`usage bot gif keyword\`
+                                                    bot rps (rock/paper scissors)
+                                                    \`usage bot rps @person\`
+                                                    bot server info
+                                                    bot my info
+                                                            `) })
+            .setTimestamp(msg.createdTimestamp)
+            .setFooter('Mera Bot By Basim');
+        msg.channel.send(embed);
+    }
+}
